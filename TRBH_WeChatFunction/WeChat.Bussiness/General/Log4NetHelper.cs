f4ae8015f5df47c7fb9624fd0cf002f7eb08b6bc
zhanggong <zm81499259@163.com>
2018-09-06 11:09:50 +0800

    增加log4net

---
+++ TRBH_WeChatFunction/WeChat.Bussiness/General/Log4NetHelper.cs
@@ -0,0 +1,55 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
+namespace WeChat.Bussiness.General
+{
+    public class Log4NetHelper
+    {
+        public static ILog customLog;
+        public static ILog execptionLog;
+        public Log4NetHelper()
+        {
+            SetConfig();
+        }
+
+        public static void SetConfig()
+        {
+            try
+            {
+                customLog = LogManager.GetLogger("CustomLoger");
+                execptionLog = LogManager.GetLogger("ExecptionLoger");
+            }
+            catch
+            {
+
+            }
+            XmlConfigurator.Configure();
+        }
+        public void SetConfig(FileInfo configFile)
+        {
+            XmlConfigurator.Configure(configFile);
+        }
+
+        public static void WriteCustomLog(string info)
+        {
+            if ((customLog != null) && customLog.IsInfoEnabled)
+            {
+                customLog.Info(info);
+            }
+        }
+
+        public static void WriteExecptionLog(string info, Exception ex)
+        {
+            if ((execptionLog != null) && execptionLog.IsErrorEnabled)
+            {
+                execptionLog.Error(info, ex);
+            }
+        }
+    }
+}
