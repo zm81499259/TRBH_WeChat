2763deffe4c3ee6f6aa195a61d43389b73f93200
zhanggong <zm81499259@163.com>
2018-09-06 13:45:02 +0800

    增加工程计划列表

---
+++ TRBH_WeChatFunction/WeChat.Application/H5Controllers/EngineerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeChat.Application.Library;
+using WeChat.Bussiness.Repository;
+using WeChat.Bussiness.RespositoryModel;
+
+namespace WeChat.Application.H5Controllers
+{
+    /// <summary>
+    /// 工程控制器
+    /// </summary>
+    [RoutePrefix("Engineer")]
+    public class EngineerController : GeneralControl<CommonRequestModel, IEngineerRepository>
+    {
+        public EngineerController() : base(new EngineerRepository()) { }
+        // GET: Engineer
+        public ActionResult Index()
+        {
+            var model = new CommonRequestModel();
+            var result = this.repository.GetEngineering(model);
+            return View(result);
+        }
+    }
+}
---
+++ TRBH_WeChatFunction/WeChat.Bussiness/Repository/IEngineerRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeChat.Bussiness.RespositoryModel;
+using WeChat.Model.DataBaseModel;
+
+namespace WeChat.Bussiness.Repository
+{
+    /// <summary>
+    /// 工程仓储
+    /// </summary>
+    public interface IEngineerRepository:IGenericRepository<CommonRequestModel>
+    {
+        /// <summary>
+        /// 获取工程数据
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        List<WX_Engineering> GetEngineering(CommonRequestModel model);
+    }
+    /// <summary>
+    /// 工程仓储
+    /// </summary>
+    public class EngineerRepository: GenericRepository<CommonRequestModel>,IEngineerRepository
+    {
+        public EngineerRepository() : base()
+        {
+
+        }
+        public EngineerRepository(ModelContext context)
+            :base(context)
+        {
+
+        }
+
+        /// <summary>
+        /// 获取数据
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public List<WX_Engineering> GetEngineering(CommonRequestModel model)
+        {
+            if(model!=null)
+            {
+                var startTime = DateTime.Now;
+                if(model.StartTime==null)
+                {
+                    model.StartTime =new DateTime(startTime.Year,startTime.Month,1,0,0,0);
+                }
+                if(model.EndTime==null)
+                {
+                    model.EndTime = model.StartTime.Value.AddMonths(1).AddDays(-1);
+                }
+                var result = this.Context.WX_Engineering.Where(x => x.CreateDate >= model.StartTime && x.CreateDate <= model.EndTime).ToList();
+                return result;
+            }
+            return null;
+        }
+    }
+}
